b582c23864ed50045bb86016d2d9908d8dfe5247
avmgithub <mendoza1@us.ibm.com>
2022-07-11 17:13:17 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
-provider "ibm" {
-  generation            = 2
-  region                = "${var.ibmcloud_region}"
+terraform {
+  # Terraform 0.15 or later is required for:
+  # * the -raw flag on `terraform output`.
+  # * the nonsensitive function
+  required_version = ">= 0.15"
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "=1.30.1"
+    }
+  }
 }
